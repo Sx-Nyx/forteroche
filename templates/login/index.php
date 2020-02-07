f847de25743bb9654716e9ee6562770c0921efd9
Sx-Nyx <kenjy.thiebault@gmail.com>
2020-02-07 14:02:56 +0100

    style(login page): add view

---
+++ templates/login/index.php
@@ -0,0 +1,12 @@
+<div class="login">
+    <div class="form__container">
+        <h2 class="light__title">Se connecter à votre compte</h2>
+        <form class="login__form">
+            <input type="text" placeholder="Identifiant" required class="login__form__input">
+            <input type="password" placeholder="Mot de passe" required class="login__form__input">
+            <div class="btn">
+                <button type="submit" class="btn__link">Se connecter</button>
+            </div>
+        </form>
+    </div>
+</div>
