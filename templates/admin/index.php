6123472e2716a0dcf4cc186ca06c3f10d965e6aa
Smiix <kenjy.thiebault@gmail.com>
2020-02-17 14:37:03 +0100

    style(admin page): add view

---
+++ templates/admin/index.php
@@ -0,0 +1,51 @@
+<div class="dashboard">
+    <div class="actions">
+        <div class="btn">
+            <a href="#" class="btn__link">Modifier la description</a>
+        </div>
+        <div class="btn">
+            <a href="#" class="btn__link">Ajouter un chapitre</a>
+        </div>
+    </div>
+    <table>
+        <thead>
+        <tr>
+            <th>Titre</th>
+            <th>Status</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Un billet Simple pour L'alaska</td>
+            <td class="active">En ligne</td>
+            <td>
+                <a href="#" class="dashboard__actions" title="Editer">
+                    <svg class="dashboard__icon">
+                        <use xlink:href="assets/images/sprite.svg#edit"></use>
+                    </svg>
+                </a> <a href="#" class="dashboard__actions" title="Supprimer">
+                    <svg class="dashboard__icon dashboard__icon-red">
+                        <use xlink:href="assets/images/sprite.svg#delete"></use>
+                    </svg>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td>Un billet Simple pour L'alaska</td>
+            <td>En attente</td>
+            <td>
+                <a href="#" class="dashboard__actions">
+                    <svg class="dashboard__icon">
+                        <use xlink:href="assets/images/sprite.svg#edit"></use>
+                    </svg>
+                </a> <a href="#" class="dashboard__actions">
+                    <svg class="dashboard__icon dashboard__icon-red">
+                        <use xlink:href="assets/images/sprite.svg#delete"></use>
+                    </svg>
+                </a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
