8d135e08ba36155891e26b90aa89dced44e3571a
Smiix <kenjy.thiebault@gmail.com>
2020-02-20 14:49:13 +0100

    style(admin): add base

---
+++ templates/base_admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Open+Sans:500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title><?= isset($title) ? $title : 'Administration | Jean-Forteroche' ?></title>
+</head>
+<body>
+<header class="header">
+    <a href="/admin" class="header__logo">Jean-Forteroche.</a>
+    <nav class="header__nav header__nav-admin">
+        <a href="/admin">Le romans</a>
+        <a href="/admin/comments">Les commentaires <span class="header__notification">3</span></a>
+    </nav>
+</header>
+<?= $content ?>
+</body>
+</html>
