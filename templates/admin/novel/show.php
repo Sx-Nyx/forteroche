f26d767c9e16ba23dec4759c34a9c64f93d7bd0d
Sx-Nyx <kenjy.thiebault@gmail.com>
2020-08-14 19:04:41 +0200

    fix(Helper\Form): remove html validation
    TinyMCE don't support html Validation

--- templates/admin/novel/show.php
+++ templates/admin/novel/show.php
@@ -3,7 +3,7 @@
         <h2 class="light__title">Modifier le roman</h2>
         <form class="novel__form" method="post">
             <?= $form->input('title', 'Titre du roman.', ['required' => true, 'minlength' => 5]) ?>
-            <?= $form->tinyMCE('description', 'Description du roman.', ['required' => true, 'minlength' => 20]) ?>
+            <?= $form->tinyMCE('description', 'Description du roman.') ?>
             <div class="btn btn--novel">
                 <button type="submit" class="btn__link">Modifier</button>
             </div>
